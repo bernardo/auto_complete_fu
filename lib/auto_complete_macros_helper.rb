859568f4ccc759d5adfae82e06fef61c88cb9fbd
Christopher Redinger <redinger@gmail.com>
2008-04-29 15:03:22 -0400

    CSRF patch

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -70,6 +70,10 @@ module AutoCompleteMacrosHelper
     js_options[:frequency]  = "#{options[:frequency]}" if options[:frequency]
     js_options[:method]     = "'#{options[:method].to_s}'" if options[:method]
 
+    if protect_against_forgery? && js_options[:method] != "'get'"
+      js_options[:parameters] = "'#{request_forgery_protection_token}=' + encodeURIComponent('#{escape_javascript form_authenticity_token}')"
+    end
+
     { :after_update_element => :afterUpdateElement, 
       :on_show => :onShow, :on_hide => :onHide, :min_chars => :minChars }.each do |k,v|
       js_options[v] = options[k] if options[k]
